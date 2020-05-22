45a88458e5b4c17406d4c534b7d12f9d66aae7b1
amber_p <panianamber@gmail.com>
2020-05-22 14:34:02 -0600

    JS for button

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-var magic8Ball = {}
-magic8Ball.listOfAnswers = ["Yes", "No", "Try Again Later", "Maybe", "Most Definetly"];
+$(document).ready(function () {
+	var magic8Ball = {}
+	magic8Ball.listOfAnswers = ["Yes", "No", "Try Again Later", "Maybe", "Most Definetly"];
+	var questionButton = document.getElementById("questionButton");
 
-magic8Ball.askQuestion = function (question) {
-	var randomNumber = Math.random();
-	var randomNumberlistOfAnswers = randomNumber * this.listOfAnswers.length;
-	var randomIndex = Math.floor(randomNumberlistOfAnswers);
-	var answer = this.listOfAnswers[randomIndex];
+	var onClick = function () {
 
-	console.log(question);
-	console.log(answer);
-};
+		var question = prompt("ASK A YES/NO QUESTION!");
+		magic8Ball.askQuestion(question);
+
+	};
+	magic8Ball.askQuestion = function (question) {
+		var randomNumber = Math.random();
+		var randomNumberlistOfAnswers = randomNumber * this.listOfAnswers.length;
+		var randomIndex = Math.floor(randomNumberlistOfAnswers);
+		var answer = this.listOfAnswers[randomIndex];
+
+		console.log(question);
+		console.log(answer);
+	};
+
+	$("#questionButton").click(onClick);
+});	
